9bfbb2ae0a7a6d652e84f4a5aecae2c17bfd30e9
Adrian Manrique <adrian1217manrique@gmail.com>
2021-12-08 15:49:20 -0800

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,1 +1,14 @@
+//import org.junit.Assert;
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+public class Test {
+  String testString = "MiniTest";
+  
+  @Test
+  public void testMake(){
+    System.out.println("Running Test1");
+    assertEquals(testString, "MiniTest", "The test failed");
+  }
+}
